3525edb4f9fa4ca75f4a37119b1aec14c1fe1b8d
Trishan214 <23da2-0214@slts.ac.lk>
2026-09-07 22:49:15 +0530

    Quesue

---
+++ HospitalEMS/src/QueueNode.java
@@ -0,0 +1,13 @@
+/**
+ * QueueNode.java
+ * A single node of the Emergency Patient Queue (linked-list based queue).
+ */
+public class QueueNode {
+    Patient patient;
+    QueueNode next;
+
+    public QueueNode(Patient patient) {
+        this.patient = patient;
+        this.next = null;
+    }
+}
