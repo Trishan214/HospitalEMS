5e690bfac87712a6cf318a9a55f047ec717d1221
Trishan214 <23da2-0214@slts.ac.lk>
2026-09-07 22:49:24 +0530

    staxk

---
+++ HospitalEMS/src/StackNode.java
@@ -0,0 +1,13 @@
+/**
+ * StackNode.java
+ * A single node of the Treatment History Stack (linked-list based stack).
+ */
+public class StackNode {
+    TreatmentRecord record;
+    StackNode next;
+
+    public StackNode(TreatmentRecord record) {
+        this.record = record;
+        this.next = null;
+    }
+}
